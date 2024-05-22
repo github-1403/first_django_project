72575e5a271add610e787c731f557c236cb36044
github-1403 <fllstck.shp@gmail.com>
2024-05-22 17:02:44 +0330

    add views for 3 pages(home , about us, contact us)

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return HttpResponse("home page")
+
+
+def about(request):
+    return HttpResponse("about page")
+
+
+def contact(request):
+    return HttpResponse("contact page")
+
